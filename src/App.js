fc915ee12ad6bff9b6443c3b77e6171aa2e17429
Sampada Naik <60545874+naiksampada5@users.noreply.github.com>
2020-06-25 21:42:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App extends Component {
             <p>(Click on the name for portfolio)</p>
             <p><h2><a href="https://sampada-portfolioo.netlify.app" >1.SAMPADA NAIK</a></h2></p>
             <p><h2><a href="https://portfolio-shreyam.netlify.app/" >2.SHREYA MISALE</a></h2></p>
+            <p><h2><a href="https://gallant-mcclintock-29e6aa.netlify.app" >3.BHAKTI PATIL</a></h2></p>
           </center>
           
         </header>
