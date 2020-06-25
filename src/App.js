387b2bb084eae21308f41563c020e5169162c03c
Sampada Naik <naiksampada.5@gmail.com>
2020-06-25 12:52:20 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+// eslint-disable-next-line
+/* eslint-disable */
+import React, { Component } from 'react';
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <center>
+            <p><h1>List of Portfolios</h1></p>
+            <p>(Click on the name for portfolio)</p>
+            <p><h2><a href="https://sampada-portfolioo.netlify.app" >1.SAMPADA NAIK</a></h2></p>
+            <p><h2><a href="https://portfolio-shreyam.netlify.app/" >2.SHREYA MISALE</a></h2></p>
+          </center>
+          
+        </header>
+       
+      </div>
+    );
+  }
+}
+export default App;
